ec59361ecb58f88be27a2116fc48811569373033
wangjonghwi <wangjh789@hanyang.ac.kr>
2021-10-16 20:58:01 +0900

    connect database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BoardModule } from './board/board.module';
+import { typeORMConfig } from './config/typeorm.config';
 
 @Module({
-  imports: [BoardModule],
+  imports: [BoardModule,
+    TypeOrmModule.forRoot(typeORMConfig)
+  ],
   controllers: [],
   providers: [],
 })
